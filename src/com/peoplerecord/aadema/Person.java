0ab7ef99cf7fab263b2bfcf656dbaa0e2eb071e1
Angi-Adema <dr.adema@gmail.com>
2026-07-01 14:58:45 -0600

    added logic to Person.java holding the variables of the user entered data such as first name, last name and age, generated the constructor initializing these fields and getter methods for the data including the toString method to print the data. In Queue.java, created a Deque object using an ArrayDeque to store user entered data, method to add a record to the deque, method to print entered records prior to sorting, and helper methods to print the sorted data based on descending last name and descending age.

--- src/com/peoplerecord/aadema/Person.java
+++ src/com/peoplerecord/aadema/Person.java
@@ -1,5 +1,36 @@
 package com.peoplerecord.aadema;
 
 public class Person {
+	
+	// Create variables to hold the person's first name, last name, and age
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	// Constructor to initialize the fields
+	public Person(String firstName, String lastName, int age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
 
+	// Generate getters for the first and last names, and age
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+	// Generate the toString() method
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
 }
--- src/com/peoplerecord/aadema/Queue.java
+++ src/com/peoplerecord/aadema/Queue.java
@@ -1,5 +1,57 @@
 package com.peoplerecord.aadema;
 
-public class Queue {
+import java.util.Deque;
+import java.util.ArrayDeque;
 
+public class Queue {
+	
+	// Create a Deque object of ArrayDeque since it grows dynamically and 
+	// is faster than LinkedList
+	private Deque<Person> deque = new ArrayDeque<>();
+	
+	// Add entered person to the deque
+	public void addPerson(Person person) {
+		
+		// Used offer() method for graceful error handling
+		deque.offer(person);
+	}
+	
+	// Print contents of the deque after user input
+	public void printUnsortedQueue() {
+		
+		// Use enhanced for loop to print contents
+		for (Person record : deque) {
+			System.out.println(record);
+		}
+	}
+	
+	// Helper methods to print descending sorted data by last name and age
+	public void descendingSortByLastName() {
+		
+		// Store the five people entered into an array
+		Person[] records = deque.toArray(new Person[5]);
+		
+		// Call quickSortLastName() passing in the array above, and lowest and 
+		// highest indexes of the array
+		quickSortLastName(records, 0, records.length - 1);
+		
+		// Use enhanced for loop to print sorted deque by last name
+		for (Person record : records) {
+			System.out.println(record);
+		}
+		
+	public void descendingSortByAge() {
+		
+		// Store the five people entered into an array
+		Person[] records = deque.toArray(new Person[5]);
+		
+		// Call quickSortAge() passing in the array above, and lowest and
+		// highest indexes of the array
+		quickSortAge(records, 0, records.length - 1);
+		
+		// Use enhanced for loop to print the sorted deque by age
+		for (Person record : records) {
+			System.out.println(record);
+		}	
+	}
 }
